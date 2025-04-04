73a9b605370256ed2b32d94c5a012da4707fe41c
WangRui <wangrui@betalpha.com>
2025-04-04 11:34:14 +0800

    最新的监控程序

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,10 +4,10 @@ export const config = {
   useMock: false,
   server: {
     dev: {
-      baseUrl: 'http://localhost:5689/ticket/backend/'
+      baseUrl: 'http://localhost:5689/ticket-monitor/backend/'
     },
     prod: {
-      baseUrl: 'http://192.168.3.109:5689/ticket/backend/'
+      baseUrl: 'http://192.168.3.109:5689/ticket-monitor/backend/'
     }
   }
 }
